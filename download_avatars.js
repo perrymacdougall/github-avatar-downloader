9a037a70c1bcfe773e419983434978c59bbb8a28
Perry <perrymacdougall@gmail.com>
2019-06-04 00:26:39 +0000

    stored access token in separate file and instructed git not to track it

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,17 @@ var request = require('request');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token 7a25748046c61748a3b8b5eaa54dea42554014f2'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
