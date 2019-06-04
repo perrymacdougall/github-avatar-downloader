9080de96f64fc1f707efdc17e8fc27b20f6054ae
Perry <perrymacdougall@gmail.com>
2019-06-04 03:43:14 +0000

    created a second HTTP request to test image download and writing stream

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,11 @@
 var request = require('request');
 var secret = require('./secrets.js');
+var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  // Creating an object to pass the url and authentication data to my HTTP request
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -31,3 +33,26 @@ getRepoContributors("jquery", "jquery", function(err, result) {
   // console.log("Errors:", err);
   // console.log("Result:", ;
 });
+
+function downloadImageByURL(url, filePath) {
+  // Using the Request module again to make another HTTP request,
+  // the relevant image and pipe it to a subdirectory
+  request.get('https://avatars2.githubusercontent.com/u/2741?v=3&s=466')
+          .on('error', function(err) {
+            throw err;
+            console.log('error block');
+          })
+          .on('response', function(response) {
+            console.log('Response Status Code: ', response.statusCode);
+            console.log('Response Status Message: ', response.statusMessage);
+            console.log('Response Headers: ', response.headers['content-type']);
+            console.log('Downloading image...');
+            console.log('response block');
+          })
+          .pipe(fs.createWriteStream('avatars/kvirani.jpg'))
+          .on('finish', function() {
+            console.log('Download complete');
+          })
+
+
+downloadImageByURL();
