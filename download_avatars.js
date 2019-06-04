b4aa152275dccef673302df8682bae09a3b00384
Perry <perrymacdougall@gmail.com>
2019-06-04 04:58:53 +0000

    rewrote parameters to accept command line arguments and wrote if statement to make them required

--- download_avatars.js
+++ download_avatars.js
@@ -2,36 +2,41 @@ var request = require('request');
 var secret = require('./secrets.js');
 var fs = require('fs');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-
 function getRepoContributors(repoOwner, repoName, cb) {
-  // Creating an object to pass the url and authentication data to my HTTP request
-  var options = {
-    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': 'token ' + secret.GITHUB_TOKEN
-    }
-  };
+  // Checking to see if required arguments have been passed
+  // If not, throws an error
+  if (process.argv[2] && process.argv[3]) {
+    console.log('Welcome to the GitHub Avatar Downloader!');
 
-  // Making an HTTP request with the Node module
-  request(options, function(err, res, body) {
-    // Parsing the JSON into an object
-    var parsedData = JSON.parse(body);
+    // Creating an object to pass the url and authentication data to my HTTP request
+    var options = {
+      url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': 'token ' + secret.GITHUB_TOKEN
+      }
+    };
 
-    // Handing back the object to my callback function
-    cb(err, parsedData);
-  });
+
+    // Making an HTTP request with the Node module
+    request(options, function(err, res, body) {
+      // Parsing the JSON into an object
+      var parsedData = JSON.parse(body);
+
+      // Handing back the object to my callback function
+      cb(err, parsedData);
+    });
+  } else {
+    throw error;
+  }
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
   // Looping over the array of objects to pull the avatar_url value
   result.forEach(function(e) {
     downloadImageByURL(e.avatar_url, e.login);
   });
 
-  // console.log("Errors:", err);
-  // console.log("Result:", ;
 });
 
 function downloadImageByURL(url, filePath) {
@@ -40,19 +45,15 @@ function downloadImageByURL(url, filePath) {
   request.get(url)
           .on('error', function(err) {
             throw err;
-            console.log('error block');
           })
           .on('response', function(response) {
             console.log('Response Status Code: ', response.statusCode);
             console.log('Response Status Message: ', response.statusMessage);
             console.log('Response Headers: ', response.headers['content-type']);
             console.log('Downloading image...');
-            console.log('response block');
           })
           .pipe(fs.createWriteStream('avatars/' + filePath + '.jpg'))
           .on('finish', function() {
             console.log('Download complete');
           })
 };
-
-// downloadImageByURL();
